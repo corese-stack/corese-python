d3c054266ca36a9a38ded27bd4bb2bf919ff8513
Jean Luc Szpyrka <Jean-Luc.Szpyrka@inria.fr>
2024-11-05 15:26:12 +0100

    separate py4j/jpype tests

---
+++ tests/test_jpype_api.py
@@ -0,0 +1,31 @@
+""" This module contains tests for the coreseAPI class."""
+
+
+import pytest
+from pycorese.api import CoreseAPI
+
+class Test_api:
+    """test user inputs"""
+
+    #  test constructor
+    def test_init_jpype_bridge(self):
+        c = CoreseAPI(java_bridge = "jpype")
+        assert(c.java_bridge == "jpype")
+
+    def test_bad_bridge(self):
+        with pytest.raises(ValueError):
+            CoreseAPI(java_bridge="user_mistake")
+
+
+    def test_bad_location_jpype(self):
+        with pytest.raises(FileNotFoundError):
+            c = CoreseAPI(corese_path="/this/file/does/not/exists",
+                          java_bridge="jpype")
+            c.loadCorese() # this will crash
+
+
+    def test_jpype_version(self):
+        c = CoreseAPI(java_bridge = "jpype")
+
+        # version not found since corese is  not loaded
+        assert(c.coreseVersion() is None)
--- tests/test_py4j_api.py
+++ tests/test_py4j_api.py
@@ -16,10 +16,6 @@ class Test_api:
         c = CoreseAPI(java_bridge = "PY4J")
         assert(c.java_bridge == "py4j")
 
-    def test_init_jpype_bridge(self):
-        c = CoreseAPI(java_bridge = "jpype")
-        assert(c.java_bridge == "jpype")
-
     def test_bad_bridge(self):
         with pytest.raises(ValueError):
             CoreseAPI(java_bridge="user_mistake")
@@ -30,9 +26,8 @@ class Test_api:
             c = CoreseAPI(corese_path="/this/file/does/not/exists")
             c.loadCorese() # this will crash
 
+    def test_py4j_version(self):
+        c = CoreseAPI(java_bridge = "PY4J")
 
-    def test_bad_location_jpype(self):
-        with pytest.raises(FileNotFoundError):
-            c = CoreseAPI(corese_path="/this/file/does/not/exists",
-                          java_bridge="jpype")
-            c.loadCorese() # this will crash
+        # version not found since corese is  not loaded
+        assert(c.coreseVersion() is None)
