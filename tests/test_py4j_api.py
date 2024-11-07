428fb005c593b00fb321cb79991c94b2144ce41c
Jean Luc Szpyrka <Jean-Luc.Szpyrka@inria.fr>
2024-11-07 15:47:20 +0100

    add getVersion() test

--- tests/test_py4j_api.py
+++ tests/test_py4j_api.py
@@ -1,11 +1,35 @@
 """ This module contains tests for the coreseAPI class."""
 
 
+import os
 import pytest
+import sys
+
 from pycorese.api import CoreseAPI
+import pycorese.corese_version as pv
 
 class Test_api:
     """test user inputs"""
+
+    # package provided/built jar file
+    # corese_path = os.path.join(os.path.abspath(os.path.join(__file__,'..', '..')),
+    #                            "build",
+    #                            "libs",
+    #                            f'corese-python-{pv.corese_version}-jar-with-dependencies.jar')
+    corese_path = os.path.abspath(os.path.join(__file__,
+                                               '..',
+                                               '..',
+                                               "build",
+                                               "libs",
+                                               f'corese-python-{pv.corese_version}-jar-with-dependencies.jar'))
+
+
+    # run before each test function
+    def setUp(self):
+        # remove CORESE_PATH, which has an effect on the loaded jar
+        os.environ.pop('CORESE_PATH', None)
+
+
 
     #  test constructor
     def test_init_default_bridge(self):
@@ -26,8 +50,16 @@ class Test_api:
             c = CoreseAPI(corese_path="/this/file/does/not/exists")
             c.loadCorese() # this will crash
 
-    def test_py4j_version(self):
-        c = CoreseAPI(java_bridge = "PY4J")
+    def test_py4j_version_none(self):
+        c = CoreseAPI()
 
         # version not found since corese is  not loaded
         assert(c.coreseVersion() is None)
+
+    def test_py4j_version_default(self):
+
+        # effectively load the corese-python jar file
+        c = CoreseAPI(corese_path = Test_api.corese_path)
+        c.loadCorese()
+
+        assert(c.coreseVersion() == pv.corese_version)
